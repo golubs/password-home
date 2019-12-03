e29087808ed3218a4feb9e03dbde214b637410e5
golubs <oleggolubs@gmail.com>
2019-12-03 12:31:22 +0100

    Read password from stdin

--- hash.js
+++ hash.js
@@ -1,10 +1,25 @@
 const fs = require("fs");
-const { askForPassword } = require("./utils/input");
+const readline = require("readline");
+
 const { hashPassword } = require("./utils/crypto");
 
-askForPassword().then(password => {
+const readlineInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readlineInterface.question("Please enter new master password:", password => {
+  // Add line break
+  readlineInterface.output.write("\n");
+
   const hashedPassword = hashPassword(password);
   fs.writeFileSync(".password", hashedPassword);
 
   console.log("New password saved");
+  readlineInterface.close();
 });
+
+// Override default output to hide password
+readlineInterface._writeToOutput = function() {
+  readlineInterface.output.write("");
+};
